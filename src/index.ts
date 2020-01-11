df6019ef21eaedaac271714af8d3020927e3dd23
Heesu Suh <hssuh96@gmail.com>
2020-01-11 15:06:33 +0900

    Add kakao auth api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express'
 
-const app = express()
+async function init() {
+  const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+  app.get('/', (req, res) => res.send('Hello World!'))
+  app.post('/auth/login/kakao', (req, res) =>
+    res.send({
+      access_token: 'fake-access-token',
+    }),
+  )
 
-app.listen(3000, () => console.log(`Example app listening on port 3000!`))
+  app.listen(3000, () => console.log(`Hotspot server listening on port 3000!`))
+}
+
+init()
