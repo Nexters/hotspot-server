e32736a207d4e6fc52f4d6ed77b479c5361555f0
Sejune Kim <sejune.kim@teamblind.com>
2020-01-26 20:01:16 +0900

    .env => config export로 변경

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,5 @@
 export const MONGO_URL =
   process.env.MONGO_URL || 'mongodb://localhost:27017/hotspot_server'
+
+export const KAKAO_SEARCH_KEY =
+  process.env.KAKAO_SEARCH_KEY || '17302d7ab7c0b14ce570be9bdb32083e'
